f78e29792d87f2ea234e55842d868e8b6cb9a738
Ajay Pradhan <pradhanajay14@gmail.com>
2022-02-28 19:17:39 +0530

    header component done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import ME from "../../assets/me.png";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'am</h5>
+        <h1>Ajay Pradhan</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
 
-export default Header
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="profile" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
