a7a7e6d7a6f3a823453560c2b830f8a416ac9bb8
Ajay Pradhan <pradhanajay14@gmail.com>
2022-02-28 23:02:29 +0530

    about section done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import ME from "../../assets/me_about.JPG";
 
 const About = () => {
   return (
-    <section id='about' >About</section>
-  )
-}
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about_container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about profile" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ Years</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>10+ </small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, vel
+            modi! Nam, nisi assumenda iste ad ex provident eius vitae saepe
+            neque. Similique adipisci non itaque, sit perspiciatis voluptatibus
+            reiciendis.
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
