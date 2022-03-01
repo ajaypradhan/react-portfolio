1ac9c315d1a4c068dda32e032af26cfaa69e5326
Ajay Pradhan <pradhanajay14@gmail.com>
2022-03-02 02:16:41 +0530

    all done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Ajay Pradhan
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#testimonial">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer__socials">
+        <a href="https://fb.com/ajapradhan02">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com/ajay.pradhan02">
+          <FaInstagram />
+        </a>
+        <a href="https://www.linkedin.com/in/ajay-pradhan-0211">
+          <FaLinkedinIn />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Ajay Pradhan, All right reserved </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
